f038558171a2e75089d254989d96a29f6fa1c99f
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2022-08-18 16:52:50 -0700

    add toggle

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
-
-
-  
-
+$(document).ready(function () {
+  $("#products").on("click", "input:checked", function (event) {
+    if ($(this).val() === "third-party") {
+      $(".third-party-delivery").removeClass("d-none");
+      $(".in-house-delivery").addClass("d-none");
+    } else {
+      $(".in-house-delivery").removeClass("d-none");
+      $(".third-party-delivery").addClass("d-none");
+    }
+  });
 });
